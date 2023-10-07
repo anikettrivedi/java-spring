5746e2eaf9f4456fec4db283d9f753f536cf2334
Aniket Trivedi <aniket@Anikets-MacBook-Pro.local>
2023-10-07 21:42:58 +0530

    spring security

--- spring-boot-web-secure/src/main/java/org/example/controllers/AuthController.java
+++ spring-boot-web-secure/src/main/java/org/example/controllers/AuthController.java
@@ -4,16 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.entity.UserRole;
 import org.example.repository.UserRolesRepository;
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("auth")
